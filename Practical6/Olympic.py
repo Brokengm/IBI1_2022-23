e1b8d4f67cbe6111c9ac1a67be3450233f16ca8e
Broken <dyq37909@outlook.com>
2023-03-22 14:45:11 +0800

    Olympic.py

--- Practical6/Olympic.py
+++ Practical6/Olympic.py
@@ -5,7 +5,9 @@ import matplotlib.pyplot as plt
 #value x y axis
 y=costs
 x=['Los Angeles 1984','Seoul 1988','Barcelona 1992','Atlanta 1996','Sydney 2000','Athens 2003','Beijing 2008','London 2012']
+#set the size of figure
 plt.figure(figsize = (9, 3))
 plt.bar(x,y)
+#make labels smaller and rotate them
 plt.xticks(fontsize = 6, rotation= 45)
 plt.show()
