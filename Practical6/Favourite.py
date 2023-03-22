0fd8e6daa51fc35f7ecfaa4373016d1691a4bf29
Broken <dyq37909@outlook.com>
2023-03-22 14:25:47 +0800

    Practical6/

--- Practical6/Favourite.py
+++ Practical6/Favourite.py
@@ -1,7 +1,12 @@
+#create a dictionary
 result = { 'Comedy': 73, 'Action':42, 'Romance':38,'Fantasy':28,'Science-fiction':22,'Horror':19,'Crime':18,'Documentary':12,'History':8,'War':7}
+#create a list of the number
 results=[73,42,38,28,22,19,18,12,8,7]
 import matplotlib.pyplot as plt
+#create pie plot
 plt.pie(results)
 plt.show()
+# give a the value of input
 a=input('input=')
+#print corresponding value
 print(result[a])
---
+++ Practical6/Olympic.py
@@ -0,0 +1,11 @@
+costs=[1,8,15,7,5,14,43,40]
+#sort the list
+costs=sorted(costs)
+import matplotlib.pyplot as plt
+#value x y axis
+y=costs
+x=['Los Angeles 1984','Seoul 1988','Barcelona 1992','Atlanta 1996','Sydney 2000','Athens 2003','Beijing 2008','London 2012']
+plt.figure(figsize = (9, 3))
+plt.bar(x,y)
+plt.xticks(fontsize = 6, rotation= 45)
+plt.show()
